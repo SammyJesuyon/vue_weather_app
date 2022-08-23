2babfe477a2b19a83a99dc61b6fddb15dc3d7b01
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-08-23 11:35:04 +0100

    updated

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -8,6 +8,14 @@
             <p class="text-3xl self-end">
                 {{ Math.round(city.weather.main.temp) }}&deg;
             </p>
+            <div class="flex gap-2">
+                <span class="text-xs">
+                    H: {{ Math.round(city.weather.main.temp_max) }}&deg;
+                </span>
+                <span class="text-xs">
+                    L: {{ Math.round(city.weather.main.temp_min) }}&deg;
+                </span>
+            </div>
         </div>
     </div>
 </template>
